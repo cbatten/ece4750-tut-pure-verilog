39a024a294aa00d0e872f5e6d48d4a4cc137aee4
cb535 <cb535@cornell.edu>
2014-09-07 11:27:21 -0400

    Updating from 0.0-289-g9a283b6-dirty

--- vc/ex-sorter-sim-flat.v
+++
@@ -1,9 +0,0 @@
-//========================================================================
-// Simulator for SorterFlat
-//========================================================================
-// Use the simulation harness to create a simulator for SorterFlat
-
-`define  EX_SORTER_IMPL ex_sorter_SorterFlat
-`include "ex-sorter-SorterFlat.v"
-`include "ex-sorter-sim-harness.v"
-
